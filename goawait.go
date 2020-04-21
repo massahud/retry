d7ce0fef072a965aa9d36665f22b6e8b71f8d126
William Kennedy <bill@ardanlabs.com>
2020-04-21 09:39:06 -0400

    fixing mutex code

--- goawait.go
+++ goawait.go
@@ -76,15 +76,17 @@ func PollAll(ctx context.Context, retryTime time.Duration, polls map[string]Poll
 	wg.Add(g)
 
 	results := make(map[string]Result)
-	var resultsLock sync.Mutex
+	var mu sync.Mutex
 
 	for name, poll := range polls {
 		name, poll := name, poll
 		go func() {
 			defer wg.Done()
-			resultsLock.Lock()
-			results[name] = Poll(ctx, retryTime, poll)
-			resultsLock.Unlock()
+			mu.Lock()
+			{
+				results[name] = Poll(ctx, retryTime, poll)
+			}
+			mu.Unlock()
 		}()
 	}
 
