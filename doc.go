21ed6599150d77fc5fddaefd8226de32c0fdbf9a
William Kennedy <bill@ardanlabs.com>
2020-04-21 09:55:45 -0400

    api naming change

--- doc.go
+++ doc.go
@@ -1,5 +1,5 @@
 /*
-Package goawait is a simple module for asynchronous waiting. Use goawait when
+Package await is a simple module for asynchronous waiting. Use goawait when
 you need to wait for asynchronous tasks to complete before continuing normal
 execution.
 
@@ -12,7 +12,7 @@ Example with polling function that returns an error:
 	poll := func(ctx context.Context) (interface{}, error) {
 		return nil, errors.New("poll error")
 	}
-	if result != goawait.Poll(ctx, 500*time.Microsecond, poll); result.Err != nil {
+	if result != await.Func(ctx, 500*time.Microsecond, poll); result.Err != nil {
 		return result.Err
 	}
 
@@ -26,7 +26,7 @@ Example simultaneously polling multiple functions:
 		return nil, fmt.Error("poll2 error")
 	}
 	polls := map[string]goawait.PollFunc{"poll1": poll1, "poll2": poll2}
-	results := goawait.PollAll(ctx, 200*time.Microsecond, polls)
+	results := await.All(ctx, 200*time.Microsecond, polls)
 	for name, result := range results {
 		fmt.Println("Name:", name, "result:", result)
 	}
@@ -43,10 +43,10 @@ Example polling until the first function returns:
 	polls := map[string]goawait.PollFunc{"faster": faster, "slower": slower}
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-	result := goawait.PollFirst(ctx, time.Millisecond, polls)
+	result := await.First(ctx, time.Millisecond, polls)
 	if result.Err != nil {
 		log.Fatal(result.Err)
 	}
 	fmt.Prinln("first result:", result.Value)
 */
-package goawait
+package await
--- goawait.go
+++ goawait.go
@@ -1,4 +1,4 @@
-package goawait
+package await
 
 import (
 	"context"
@@ -38,9 +38,9 @@ func (err *Error) Unwrap() error {
 	return err.errPoll
 }
 
-// Poll calls the poll function every retry interval until the poll
+// Func calls the poll function every retry interval until the poll
 // function succeeds or the context times out.
-func Poll(ctx context.Context, retryInterval time.Duration, poll PollFunc) Result {
+func Func(ctx context.Context, retryInterval time.Duration, poll PollFunc) Result {
 	var retry *time.Timer
 	start := time.Now()
 
@@ -68,9 +68,9 @@ func Poll(ctx context.Context, retryInterval time.Duration, poll PollFunc) Resul
 	}
 }
 
-// PollAll calls all the poll functions every retry interval until the poll
+// All calls all the poll functions every retry interval until the poll
 // functions succeeds or the context times out.
-func PollAll(ctx context.Context, retryTime time.Duration, polls map[string]PollFunc) map[string]Result {
+func All(ctx context.Context, retryTime time.Duration, polls map[string]PollFunc) map[string]Result {
 	g := len(polls)
 	var wg sync.WaitGroup
 	wg.Add(g)
@@ -84,7 +84,7 @@ func PollAll(ctx context.Context, retryTime time.Duration, polls map[string]Poll
 			defer wg.Done()
 			mu.Lock()
 			{
-				results[name] = Poll(ctx, retryTime, poll)
+				results[name] = Func(ctx, retryTime, poll)
 			}
 			mu.Unlock()
 		}()
@@ -95,10 +95,10 @@ func PollAll(ctx context.Context, retryTime time.Duration, polls map[string]Poll
 	return results
 }
 
-// PollFirst calls all the poll functions every retry interval until the poll
+// First calls all the poll functions every retry interval until the poll
 // functions succeeds or the context times out. Once the first polling function
 // the succeeds returns, this function will return that result.
-func PollFirst(ctx context.Context, retryTime time.Duration, polls map[string]PollFunc) Result {
+func First(ctx context.Context, retryTime time.Duration, polls map[string]PollFunc) Result {
 	start := time.Now()
 	g := len(polls)
 	results := make(chan Result, g)
@@ -109,7 +109,7 @@ func PollFirst(ctx context.Context, retryTime time.Duration, polls map[string]Po
 	for _, poll := range polls {
 		poll := poll
 		go func() {
-			results <- Poll(ctx, retryTime, poll)
+			results <- Func(ctx, retryTime, poll)
 		}()
 	}
 
